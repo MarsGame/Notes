9ed4119faf81b74647d11d60a8280565d4d46057
fishkq <cs_quan@qq.com>
2017-08-19 12:22:39 +0800

    First commit

---
+++ Notes/ViewController.swift
@@ -0,0 +1,43 @@
+//
+//  ViewController.swift
+//  Notes
+//
+//  Created by YuKunquan on 27/07/2017.
+//  Copyright © 2017 YuKunquan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        let bounds = self.view.bounds
+        
+        UIGraphicsBeginImageContextWithOptions(bounds.size, false, UIScreen.main.scale)
+        
+        let context = UIGraphicsGetCurrentContext()
+        context?.saveGState()
+        context?.translateBy(x: -self.view.frame.origin.x, y: -self.view.frame.origin.y);
+        
+        self.view.layer.render(in: context!)
+        
+        let image = UIGraphicsGetImageFromCurrentImageContext()
+        
+        context?.restoreGState()
+        UIGraphicsEndImageContext()
+        
+        print(image!)
+        
+        UIImageWriteToSavedPhotosAlbum(image!, nil, nil, nil)
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
